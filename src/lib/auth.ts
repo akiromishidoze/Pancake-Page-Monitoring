5110c15ead49f20dbbe1b8557fed7125392e53d9
teccjm <teccjm@users.noreply.github.com>
2026-06-01 08:23:26 +0800

    chore: migrate from bcrypt to bcryptjs dependency

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import { getSetting, setSetting, createSessionToken, validateSessionToken, clearSessionToken } from './db';
 import { createLogger } from './logger';
 
